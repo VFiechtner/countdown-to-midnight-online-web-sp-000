28413959965ade4ebcf3bf7a10b374c561fd5642
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-04-19 19:39:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,15 @@ def countdown(seconds)
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
   end
+  return "HAPPY NEW YEAR!"
+end
 
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
   return "HAPPY NEW YEAR!"
 end
